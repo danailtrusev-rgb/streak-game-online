a22cb2c464340f9016d44431e1d1b24937074488
danailtrusev-rgb <danail.t.rusev@gmail.com>
2026-07-14 16:50:54 +0300

    Deleted vite-env.d.ts

--- src/hooks/useTestMode.ts
+++
@@ -1,6 +0,0 @@
-import { useMemo } from 'react';
-import { parseTestMode, type TestModeConfig } from '../lib/testMode';
-
-export function useTestMode(): TestModeConfig {
-  return useMemo(() => parseTestMode(), []);
-}
--- src/pages/games/PickPage.tsx
+++
@@ -1,5 +0,0 @@
-import MicrogamePage from '../../components/game/MicrogamePage';
-
-export default function PickPage() {
-  return <MicrogamePage gameId="daily_pick" />;
-}
